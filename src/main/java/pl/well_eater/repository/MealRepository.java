ac8bec70c4d6be2e9e140ea2387d177852db5147
Daniel Delegacz <ddelegacz@gmail.com>
2024-07-19 23:49:52 +0200

    add repository for meal

---
+++ src/main/java/pl/well_eater/repository/MealRepository.java
@@ -0,0 +1,7 @@
+package pl.well_eater.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.well_eater.model.MealEntity;
+
+public interface MealRepository extends JpaRepository<MealEntity, Long> {
+}
